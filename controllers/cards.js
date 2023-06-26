678dbb7c87f7f22087ba8a87c74b68dc2985db0a
MVTT <ryabtzun2011@yandex.ru>
2023-06-26 13:33:50 +0300

    feat: backend

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,21 +1,41 @@
+const mongoose = require('mongoose');
+const { codesError } = require('../const');
 const Card = require('../models/card');
 
 const getCards = (req, res) => {
-  Card.find({}).then((cards) => res.send({ data: cards }));
+  Card.find({})
+    .then((cards) => res.send({ data: cards }))
+    .catch(() => {
+      res.status(codesError.DEFAULT).send({ message: 'Ошибка по-умолчанию' });
+    });
 };
 
 const postCard = (req, res) => {
   const { name, link } = req.body;
 
-  Card.create({ name, link, owner: req.user._id }).then((card) => {
-    res.send({ data: card });
-  });
+  Card.create({ name, link, owner: req.user._id })
+    .then((card) => res.send({ data: card }))
+    .catch((err) => {
+      if (err instanceof mongoose.Error.ValidationError) {
+        res.status(codesError.INCORRECT_DATA).send({
+          message: 'Переданы некорректные данные при создании карточки',
+        });
+      } else {
+        res.status(codesError.DEFAULT).send({ message: 'Ошибка по-умолчанию' });
+      }
+    });
 };
 
 const deleteCard = (req, res) => {
-  Card.findByIdAndRemove(req.params.cardId).then((card) => {
-    res.send({ data: card });
-  });
+  Card.findByIdAndRemove(req.params.cardId)
+    .then((card) => res.send({ data: card }))
+    .catch((err) => {
+      if (err instanceof mongoose.Error.DocumentNotFoundError) {
+        res
+          .status(codesError.NOT_FOUND_DATA)
+          .send({ message: 'Карточка с указанным _id не найдена' });
+      }
+    });
 };
 
 const putLikeCard = (req, res) => {
@@ -23,7 +43,21 @@ const putLikeCard = (req, res) => {
     req.params.cardId,
     { $addToSet: { likes: req.user._id } },
     { new: true }
-  ).then((card) => res.send({ data: card }));
+  )
+    .then((card) => res.send({ data: card }))
+    .catch((err) => {
+      if (err instanceof mongoose.Error.ValidationError) {
+        res.status(codesError.INCORRECT_DATA).send({
+          message: 'Переданы некорректные данные для постановки лайка',
+        });
+      } else if (err instanceof mongoose.Error.DocumentNotFoundError) {
+        res
+          .status(codesError.NOT_FOUND_DATA)
+          .send({ message: 'Передан несуществующий _id карточки' });
+      } else {
+        res.status(codesError.DEFAULT).send({ message: 'Ошибка по-умолчанию' });
+      }
+    });
 };
 
 const deleteLikeCard = (req, res) => {
@@ -31,7 +65,21 @@ const deleteLikeCard = (req, res) => {
     req.params.cardId,
     { $pull: { likes: req.user._id } },
     { new: true }
-  ).then((card) => res.send({ data: card }));
+  )
+    .then((card) => res.send({ data: card }))
+    .catch((err) => {
+      if (err instanceof mongoose.Error.ValidationError) {
+        res.status(codesError.INCORRECT_DATA).send({
+          message: 'Переданы некорректные данные для снятия лайка',
+        });
+      } else if (err instanceof mongoose.Error.DocumentNotFoundError) {
+        res
+          .status(codesError.NOT_FOUND_DATA)
+          .send({ message: 'Передан несуществующий _id карточки' });
+      } else {
+        res.status(codesError.DEFAULT).send({ message: 'Ошибка по-умолчанию' });
+      }
+    });
 };
 
 module.exports = {
--- controllers/users.js
+++ controllers/users.js
@@ -1,23 +1,61 @@
+const mongoose = require('mongoose');
+const { codesError } = require('../const');
 const User = require('../models/user');
 
 const getUsers = (req, res) => {
-  User.find({}).then((users) => res.send({ data: users }));
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch(() => {
+      res.status(codesError.DEFAULT).send({ message: 'Ошибка по-умолчанию' });
+    });
 };
 
 const getUser = (req, res) => {
-  User.findById(req.params.userId).then((user) => res.send({ data: user }));
+  User.findById(req.params.userId)
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err instanceof mongoose.Error.DocumentNotFoundError) {
+        res
+          .status(codesError.NOT_FOUND_DATA)
+          .send({ message: 'Пользователь по указанному _id не найден' });
+      } else {
+        res.status(codesError.DEFAULT).send({ message: 'Ошибка по-умолчанию' });
+      }
+    });
 };
 
 const postUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
-  User.create({ name, about, avatar }).then((user) => res.send({ data: user }));
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err instanceof mongoose.Error.ValidationError) {
+        res.status(codesError.INCORRECT_DATA).send({
+          message: 'Переданы некорректные данные при создании пользователя',
+        });
+      } else {
+        res.status(codesError.DEFAULT).send({ message: 'Ошибка по-умолчанию' });
+      }
+    });
 };
 
 const patchUser = (req, res, data) => {
-  User.findByIdAndUpdate(req.user._id, data).then((user) => {
-    res.send({ data: user });
-  });
+  User.findByIdAndUpdate(req.user._id, data)
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err instanceof mongoose.Error.ValidationError) {
+        res.status(codesError.INCORRECT_DATA).send({
+          message: 'Переданы некорректные данные при обновлении профиля',
+        });
+      } else if (err instanceof mongoose.Error.DocumentNotFoundError) {
+        res
+          .status(codesError.NOT_FOUND_DATA)
+          .send({ message: 'Пользователь с указанным _id не найден' });
+      } else {
+        res.status(codesError.DEFAULT).send({ message: 'Ошибка по-умолчанию' });
+      }
+    });
 };
 
 const patchUserProfile = (req, res) => {
